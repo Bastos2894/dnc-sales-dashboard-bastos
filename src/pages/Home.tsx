2e074cc5f0c054fe719406e04553f3c1acd83d81
bastos28 <bastos.pinheiro94@gmail.com>
2026-01-08 10:15:39 -0400

    feat: Avatars List Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,35 @@
-import {Header, CardComponent} from 'src/components'
+import {Header, CardComponent, AvatarsList} from 'src/components'
 import { Container } from '@mui/material'
+import { currencyConverter }  from 'src/utils'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/Avatar-DNC.svg',
+      name: 'Nice dd',
+      subtitle: currencyConverter(3568),
+    },
+    {
+      avatar: '/Avatar-DNC.svg',
+      name: 'Nice dd2',
+      subtitle: currencyConverter(144552.55),
+    },
+    {
+      avatar: '/Avatar-DNC.svg',
+      name: 'Nice dd3',
+      subtitle: currencyConverter(156884.55),
+    }
+  ]
+
   return (
     <>
       <Header/>
       <Container maxWidth="lg">
         <CardComponent>
           Card
+        </CardComponent>
+        <CardComponent>
+          <AvatarsList listData={mockListData}/>
         </CardComponent>
       </Container>
       
